7547535cfc4b2e970b42f6274759dd28043225c6
Agarova Uliana <uagarova@gmail.com>
2023-05-06 22:33:54 +0800

    mole-game добавила обнуление счетчика после раунда и проверку на возможность несколько раз кликать в одной ячейке

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,6 +2,7 @@ const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 let clickCount = 0;
 let lostCount = 0;
+let lastClickedPosition = null;
 
 const getHole = index => document.getElementById(`hole${index}`);
 
@@ -12,6 +13,11 @@ for (let i = 1; i <= 9; i++) {
         if (hole.classList.contains('hole_has-mole')) {
             clickCount++;
             dead.innerHTML = clickCount;
+            if (lastClickedPosition === i) { // если кликнули на ту же ячейку, где уже был крот
+                clickCount++; // увеличиваем счетчик еще на 1
+                dead.innerHTML = clickCount;
+            }
+            lastClickedPosition = i;
         } else {
             lostCount++;
             lost.innerHTML = lostCount;
@@ -21,13 +27,19 @@ for (let i = 1; i <= 9; i++) {
             alert("Победа!");
             clickCount = 0;
             lostCount = 0;
+            lost.innerHTML = 0;
+            dead.innerHTML = 0; 
+            lastClickedPosition = null;
         }
         
         if (lostCount === 5) {
             alert("Вы проиграли");
             clickCount = 0;
             lostCount = 0;
+            lost.innerHTML = 0;
+            dead.innerHTML = 0; 
+            lastClickedPosition = null;
         }
+        //hole.onclick = null;
     }
 }
-
