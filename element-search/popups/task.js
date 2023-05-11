0f87a84c5d4667de8e6d537a9012cab251859f30
Agarova Uliana <uagarova@gmail.com>
2023-05-11 22:34:52 +0800

    поправила работу второго окна

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,14 +3,26 @@ popup.classList.add("modal_active");
 
 const modalSuccess = document.querySelector("#modal_success");
 const modalClose = document.querySelectorAll(".modal__close");
+const successModalClose = document.querySelectorAll("#modal_success .modal__close_times");
 
 modalClose.forEach(function (item) {
     item.addEventListener("click", function () {
-      popup.classList.remove("modal_active");
+        const modal = item.closest('.modal');
+        modal.classList.remove("modal_active");
     });
 });
 
-modalSuccess.addEventListener("click", function() {
-    modalSuccess.classList.remove("modal_active");
-    popup.classList.add("modal_active");
+const showSuccessButton = document.querySelector(".show-success");
+showSuccessButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    popup.classList.remove("modal_active");
+    const modalSuccess = document.getElementById("modal_success");
+    modalSuccess.classList.add("modal_active");
+});
+
+successModalClose.forEach(function(item) {
+    item.addEventListener("click", function() {
+        const modalSuccess = document.getElementById("modal_success");
+        modalSuccess.classList.remove("modal_active");
+    });
 });
