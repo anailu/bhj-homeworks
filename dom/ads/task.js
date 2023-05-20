c8fa38f1229cac7aa91c2c81af8a116d7cb65711
Agarova Uliana <uagarova@gmail.com>
2023-05-20 19:06:49 +0800

    first commit book-reader

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,23 @@
 const rotatorCase = document.querySelectorAll(".rotator__case");
 let currentActiveIndex = 0;
+let speed = 0;
+let interval;
 
-setInterval(() => {
+const rotateElement = () => {
     const prevActiveIndex = currentActiveIndex;
-    const nextActiveIndex = (currentActiveIndex + 1) % rotatorCase.length;
+    const nextActiveIndex = currentActiveIndex === rotatorCase.length - 1 ? 0 : currentActiveIndex + 1;
 
     rotatorCase[prevActiveIndex].classList.remove("rotator__case_active");
     rotatorCase[nextActiveIndex].classList.add("rotator__case_active");
 
     currentActiveIndex = nextActiveIndex;
-}, 1000);
+
+    const currentElement = rotatorCase[currentActiveIndex];
+    currentElement.style.color = currentElement.dataset.color;
+    speed = currentElement.dataset.speed;
+
+    clearInterval(interval);
+    interval = setInterval(rotateElement, speed);
+};
+
+interval = setInterval(rotateElement, speed);
