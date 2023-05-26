66712a25d078caea2c5c30c638f1588c616859f7
Agarova Uliana <uagarova@gmail.com>
2023-05-26 16:13:09 +0800

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,36 @@
+const input = document.getElementById("task__input");
+const button = document.getElementById("tasks__add");
+const taskList = document.getElementById("tasks__list");
+
+function addTask(title) {
+    const task = document.createElement("div");
+    task.classList.add("task");
+
+    const taskTitle = document.createElement("div");
+    taskTitle.classList.add("task-title");
+    taskTitle.textContent = title;
+    task.appendChild(taskTitle);
+    
+    const removeBtn = document.createElement("a");
+    removeBtn.href = "#";
+    removeBtn.classList.add("task_remove");
+    removeBtn.innerHTML = "&times;";
+    removeBtn.addEventListener("click", () => {
+        task.remove();
+    });
+    task.appendChild(removeBtn);
+
+    return task;
+}
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const inputValue = input.value.trim();
+    if (inputValue.length > 0) {
+        const task = addTask(inputValue);
+        taskList.appendChild(task);
+        input.value = "";
+    } else {
+        alert("Содержание задачи не может быть пустым");
+    }
+})
