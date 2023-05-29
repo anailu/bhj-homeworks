89503f4e86f9764761248c4312dc4eebac854f62
Agarova Uliana <uagarova@gmail.com>
2023-05-29 18:19:32 +0800

    first commit poll

---
+++ async-requests/poll/task.js
@@ -0,0 +1,44 @@
+function getData(callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                callback(data);
+            } else {
+                console.error("Ошибка при получении данных опроса:", xhr.status);
+            }
+        }
+    };
+    xhr.send();
+}
+
+function showQstn(data) {
+    const pollTitle = document.getElementById("poll__title");
+    const pollAnswers = document.getElementById("poll__answers");
+
+    pollTitle.innerText = data.title;
+
+    data.answers.forEach(answer => {
+        const answerBtn = document.createElement("button");
+        answerBtn.className = "poll__answer";
+        answerBtn.innerText = answer;
+        answerBtn.addEventListener("click", () => {
+            showConfirmationDialog();
+        });
+        pollAnswers.appendChild(answerBtn);
+    });
+}
+
+function showConfirmationDialog() {
+    alert("Спасибо, ваш голос засчитан!");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    getData(function(pollData) {
+        showQstn(pollData.data);
+    });
+});
+
+
