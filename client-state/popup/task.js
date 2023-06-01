5f25019babae8944893d5abd401f9bad9f0c51e8
Agarova Uliana <uagarova@gmail.com>
2023-06-01 17:03:13 +0800

    first commit popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,17 +1,16 @@
 const modal = document.getElementById("subscribe-modal");
-const closeBtn = document.querySelector("div.modal__close");
+const closeBtn = document.querySelector(".modal__close");
 
-const getCookie = (pair) => {
-    const pairs = document.cookie.split("; ");
-    return pairs.includes(pair);
+function getCookie(cookieToCheck) {
+    const nameCookie = document.cookie.split("; ");
+    return nameCookie.includes(cookieToCheck);
 }
+
+closeBtn.addEventListener("click", function() {
+    modal.classList.remove("modal_active");
+    document.cookie = "flag=1";
+});
 
 if (!getCookie("flag=1")) {
     modal.classList.add("modal_active");
-} 
-
-closeBtn.onclick = () => {
-    console.log("клик");
-    modal.classList.remove("modal_active");
-    document.cookie = "flag=1";
 }
